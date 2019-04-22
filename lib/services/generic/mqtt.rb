19265926657f1c8af40f5a9c1f7a9a06acb0c0a3
pofystyy <tanya.romas@gmail.com>
2019-04-22 18:30:43 +0300

    modify method .deliver for direct and broadcast send

--- lib/services/generic/mqtt.rb
+++ lib/services/generic/mqtt.rb
@@ -1,3 +1,4 @@
+require_relative '../../../config'
 require 'mqtt'
 
 module Mqtt
@@ -7,23 +8,36 @@ module Mqtt
     class MissingParamException < BaseException; end
 
     def self.deliver(msg:)
-      new.deliver(topic: msg[:topic], message: msg[:message])
+      new.deliver(message: msg[:message], device_token: msg[:device_token], topic: msg[:topic])
     end
 
     def initialize
       @client = MQTT::Client.connect('localhost')
     end
 
-    def deliver(topic:, message:)
-      raise(MissingParamException, "error: Topic name cannot be empty") if topic.nil? || topic.empty?
+    def deliver(message:, device_token: nil, topic: nil)
+      raise(MissingParamException, "error: Topic name cannot be empty") if device_token.nil? && topic.nil?
+      to = device_token.nil? ? topic + config[:mqtt][:topic] : config[:mqtt][:device_token] + device_token
 
-      process_result(@client.publish(topic, message, retain=false))
+      process_result(@client.publish(payload(to: to, message: message)))
     end
+
+    def payload(to:, message:)
+      {
+        to: to,
+        message: message
+      }
+    end
+
 
     def process_result(response)
       return true if response.nil?
 
       raise DeliverException
+    end
+
+    def config
+      Mqtt::Config.new(:mqtt).config
     end
   end
 end
